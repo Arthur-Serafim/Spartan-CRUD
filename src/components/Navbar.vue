c13a0b94cb874108cbc33c66a22b51cca0ef471f
Seraph-Arthur <dev.seraph.arthur@gmail.com>
2019-03-24 20:40:11 -0300

    idk

---
+++ src/components/Navbar.vue
@@ -0,0 +1,22 @@
+<template>
+    <nav>
+        <v-toolbar>
+            <v-toolbar-title>Ninja Smoothies</v-toolbar-title>
+        </v-toolbar>
+    </nav>
+</template>
+
+<script>
+export default {
+    components: {  },
+    data() {
+        return {
+           
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+
+</style>
